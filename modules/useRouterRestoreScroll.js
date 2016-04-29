959ae30990b4244785afe54276a4d032463974a0
Ryan Florence <rpflorence@gmail.com>
2016-04-29 15:38:42 -0700

    what are we gonna do with you eslint-plugin-react?

--- modules/useRouterRestoreScroll.js
+++ modules/useRouterRestoreScroll.js
@@ -1,4 +1,7 @@
 import React from 'react'
+
+// getting that dumb warning even though it's used in the JSX!
+/*eslint no-unused-vars: 0 */
 import RestoreWindowScroll from './RestoreWindowScroll'
 
 const useRouterRestoreScroll = () => ({
